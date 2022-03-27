4dabc3fffd8a56a84fc786eaac1bb3068862ad6f
Andrew Porter <partydrone@icloud.com>
2022-03-27 02:28:58 -0500

    Prevent anonymous user pseudonym

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  validate :pseudonym_is_not_anonymous
+
+  private
+
+  def pseudonym_is_not_anonymous
+    errors.add(:pseudonym, "can't be anonymous") if pseudonym.match(/anonymous/i)
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,4 +6,10 @@ describe User do
   it "has a pseudonym" do
     _(subject).must_respond_to :pseudonym
   end
+
+  it "does not allow anonymous pseudonym" do
+    subject.pseudonym = "Anonymous"
+
+    _(subject).wont_be :valid?
+  end
 end
